b25b635d00f23f9ec36bd43cd9c39f078ea2bf29
veyoscot <vikieyo20@gmail.com>
2023-06-16 09:55:51 -0700

    My hello4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
-#include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+#include <stdio.h>
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
+}
