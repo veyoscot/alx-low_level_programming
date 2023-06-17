cc973173a676e0863b57611264871cdccd8bc68b
veyoscot <vikieyo20@gmail.com>
2023-06-16 17:21:40 -0700

    My variables3 commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char alphabet;
+
+    for (alphabet = 'A'; alphabet <= 'Z' ; alphabet++)
+    {
+      for  (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+        putchar(alphabet);
+
+    }
+
+    putchar('\n');
+    return (0);
+}
