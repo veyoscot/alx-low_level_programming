ee5ba7a31e072530f4b6960b8759db46487a6179
veyoscot <vikieyo20@gmail.com>
2023-06-21 05:16:52 -0700

    My main commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,16 @@
-#ifndef main.h
-#declare main.h
+#ifndef MAIN_HEADER FILE
+#define MAIN_HEADER FILE
 
 int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+
+#endif
