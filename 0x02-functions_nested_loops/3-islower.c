74fc0fddf9ca4f74dd75a0df9c24f359f9d9fcea
veyoscot <vikieyo20@gmail.com>
2023-06-21 19:13:21 -0700

    My nest3 commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,16 @@
 #include "main.h"
 
 /**
- *main - entry point
- *
- *Description: checks for alphabetic character
- *@c: is the char to be checked
- *
- *Return: 1 if char is lowercase, otherwise 0
+ * _islower - check if char is lowercase
+ * @c: is the char to be checked
+ * Return: 1 if char is lowercase, otherwise 0.
  */
-int _isalpha(int c)
+
+int _islower(int c)
 {
-if (c >= 97 && c <= 122)
-{
-return (1);
-}
-else
-{
-return (0);
-}
-_putchar('\n');
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+		return (0);
 }
